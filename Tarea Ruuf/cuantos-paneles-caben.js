d16c87bf8e5a377dd9b9310008f258fc0bfffa1a
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-14 18:33:08 -0400

    Add solution

--- Tarea Ruuf/cuantos-paneles-caben.js
+++ Tarea Ruuf/cuantos-paneles-caben.js
@@ -1,11 +1,28 @@
-function cuantosPanelesCaben() {
-  // A es un rectangulo de x por y
-  // B es un panel de a por b
+function cuantosPanelesCaben(baseA, alturaA, baseB, alturaB) {
+  let rectangulo = baseA * alturaA;
+  let panel = baseB * alturaB;
+  
+  if (rectangulo / panel === 1) {
+    return "Cabe un panel";
+  }
+  else if (rectangulo / panel < 1) {
+    return "No cabe ningún panel";
+  }
+  else { 
+    return "Caben " + Math.floor(rectangulo / panel) + " paneles";
+  }
+  // Implementar la función cuantosPanelesCaben que reciba 4 parámetros
 
-  // Calculo de area de rectángulo A = x por y  
-  // Calculo de area de panel B = a por b  
+  // A es un rectangulo de baseA por alturaA
+  // B es un panel de baseB por alturaB
 
-  // Area de panel A entre Area de rectangulo B = Cantidad de paneles que caben en A
+  // Calculo de area de rectángulo A = baseA por alturaA  
+  // Calculo de area de panel B = baseB por alturaB  
+
+  // Area de rectangulo A entre Area de panel B = Cantidad de paneles que caben en A
 
   // Retornar la cantidad de paneles que caben en A
 }
+
+let Resultado = cuantosPanelesCaben(166, 68, 1000, 800);
+console.log(Resultado);
