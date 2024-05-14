1b2766c91c440e139d8cfe618c46b1502c692b02
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-14 17:40:00 -0400

    Add some pseudo-code

--- Tarea Ruuf/cuantos-paneles-caben.js
+++ Tarea Ruuf/cuantos-paneles-caben.js
@@ -1,2 +1,11 @@
 function cuantosPanelesCaben() {
+  // A es un rectangulo de x por y
+  // B es un panel de a por b
+
+  // Calculo de area de x por y = Area de A
+  // Calculo de area de a por b = Area de B
+
+  // Area de panel A entre Area de rectangulo B = Cantidad de paneles que caben en A
+
+  // Retornar la cantidad de paneles que caben en A
 }
